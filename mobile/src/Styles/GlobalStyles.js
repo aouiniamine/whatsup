568995cadbdc89c12a8e5de7736f4bb2e0f5158f
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-19 16:18:45 +0100

    global name style

--- mobile/src/Styles/GlobalStyles.js
+++ mobile/src/Styles/GlobalStyles.js
@@ -1,6 +1,11 @@
-import {Dimensions, StyleSheet} from 'react-native';
+import {Dimensions, } from 'react-native';
 export const lightgreen = "#acf2bb"
 export const darkgreen = "#53ad77"
 
 export const windowWidth = Dimensions.get('window').width;
 export const windowHeight = Dimensions.get('window').height;
+
+export const nameStyle = {
+    fontSize: 18,
+    fontWeight: "500"
+}
