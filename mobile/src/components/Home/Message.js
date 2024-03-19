75a7dc615d2e655c4e9e550f040805702311f765
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-19 16:18:34 +0100

    setup message display with neat style

---
+++ mobile/src/components/Home/Message.js
@@ -0,0 +1,50 @@
+import { StyleSheet, View, Text } from "react-native"
+import { Avatar } from 'react-native-elements';
+import { nameStyle, windowWidth } from "../../Styles/GlobalStyles";
+import { useEffect, useState } from "react";
+const Message = ({message, name, image, }) =>{
+    const [showMessage, setShowMessage] = useState(message)
+    useEffect(()=>{
+        if (message.length > 27){
+            let msg = message.slice(0, 27).concat("...")
+            setShowMessage(msg)
+        }
+    }, [])
+    return (
+        <View style={style.container}>
+            <Avatar
+                rounded
+                size={70}
+                source={{
+                    uri:
+                    'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+                }}
+            />
+            <View style={{marginLeft: 20, marginRight: 90, marginTop: 15}}>
+                <Text style={nameStyle}>{name}</Text>
+                <Text style={style.message}>{showMessage}</Text>
+
+            </View>
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginLeft: 20,
+        flex: 1,
+        flexDirection: "row",
+        paddingVertical: 10
+    },
+    message: {
+
+        color: "grey",
+        fontSize: 16,
+        fontWeight: "400"
+    }
+
+})
+
+export default Message
