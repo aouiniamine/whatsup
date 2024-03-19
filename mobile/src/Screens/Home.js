9cfc23ae43a414af370a4432a478618b3fa532aa
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-19 16:19:08 +0100

    display message dummy data & style

--- mobile/src/Screens/Home.js
+++ mobile/src/Screens/Home.js
@@ -1,9 +1,19 @@
+import { FlatList } from "react-native"
 import NavBar from "../components/atoms/NavBar"
+import Message from "../components/Home/Message"
 
 const Home = () =>{
+
+    const dummyChats = [
+        {message: "Heyyyyy!ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", name: "Jhon Doe", image: ""}, 
+        {message: "Heyy I'd love tooo!!", name: "Diana Doe", image: ""}
+    ]
+
     return (
         <>
             <NavBar></NavBar>
+            <FlatList data={dummyChats}
+            renderItem={({item, i}) => <Message key={i} name={item.name} message={item.message}/>}/>
         </>
     )
 }
