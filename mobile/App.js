69fc341edd3cd27592558a95e5e27b88734d06b3
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-19 23:02:34 +0100

    initialize navigation

--- mobile/App.js
+++ mobile/App.js
@@ -2,20 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Screens/Home';
 import { lightgreen } from './src/Styles/GlobalStyles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Connect from './src/Screens/Connect';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home></Home>
-      
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Connect'>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Connect" component={Connect} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: "lightgreen",
-    
-  },
-});
