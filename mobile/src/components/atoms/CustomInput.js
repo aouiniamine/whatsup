6744b354099f00be0121b93b5d1156c83f3ec3b7
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-19 23:01:37 +0100

    setup custom input

---
+++ mobile/src/components/atoms/CustomInput.js
@@ -0,0 +1,20 @@
+import { StyleSheet, TextInput } from "react-native";
+import { darkgreen, windowWidth } from "../../Styles/GlobalStyles";
+
+export default function CustomInput ({value="", placeholder, onChange, style=null}) {
+    return (
+        <TextInput placeholder={placeholder} defaultValue={value}
+        style={[style1.input, style]} onChangeText={onChange}/>
+    )
+}
+
+const style1 = StyleSheet.create({
+    input: {
+        borderColor: darkgreen,
+        padding: 15,
+        borderWidth: 3,
+        borderRadius: 50,
+        backgroundColor: "white",
+        width: windowWidth * 0.8
+    }
+})
