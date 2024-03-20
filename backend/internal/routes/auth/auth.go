9a106a842587b3b7c063e41e51a4c5f1ad5aec30
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-20 19:22:23 +0100

    init handler

--- backend/internal/routes/auth/auth.go
+++ backend/internal/routes/auth/auth.go
@@ -1,15 +1,36 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type ConnectBody struct {
+	Credential string `json:"credential"`
+}
+
 func AuthRouter() http.Handler {
 	r := mux.NewRouter()
-	r.HandleFunc("/auth/signup", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("api works"))
+	r.HandleFunc("/auth/connect", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("request has came!!")
+		decoder := json.NewDecoder(r.Body)
+		var body ConnectBody
+		err := decoder.Decode(&body)
+		fmt.Println(body.Credential)
+		if err != nil {
+
+			panic(err)
+		}
+		jsonRes, err := json.Marshal(body)
+		if err != nil {
+
+			panic(err)
+		}
+
+		w.Write(jsonRes)
 	}).Methods("POST")
 	return r
 }
