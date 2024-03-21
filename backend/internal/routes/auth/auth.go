12e20485627695a337a546054d6ba608b69c6c28
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-21 17:12:10 +0100

    finished auth connect search  user in db

--- backend/internal/routes/auth/auth.go
+++ backend/internal/routes/auth/auth.go
@@ -1,10 +1,13 @@
 package auth
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/aouiniamine/whatsup/backend/internal/organisms/db"
+	"github.com/aouiniamine/whatsup/backend/internal/organisms/errors"
 	"github.com/gorilla/mux"
 )
 
@@ -12,25 +15,43 @@ type ConnectBody struct {
 	Credential string `json:"credential"`
 }
 
+type User struct {
+	Id    int    `db:"id" json:"id"`
+	Name  string `db:"username" json:"username"`
+	Email string `db:"email" json:"email"`
+}
+
 func AuthRouter() http.Handler {
 	r := mux.NewRouter()
-	r.HandleFunc("/auth/connect", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("request has came!!")
-		decoder := json.NewDecoder(r.Body)
-		var body ConnectBody
-		err := decoder.Decode(&body)
-		fmt.Println(body.Credential)
-		if err != nil {
-
-			panic(err)
-		}
-		jsonRes, err := json.Marshal(body)
-		if err != nil {
-
-			panic(err)
-		}
-
-		w.Write(jsonRes)
-	}).Methods("POST")
+	r.HandleFunc("/auth/connect", connect).Methods("POST")
 	return r
+}
+
+func connect(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("request has came!!")
+	var body ConnectBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		errors.InternalServerError(w, r)
+		return
+	}
+	db := db.DBConnection
+	var user User
+	row := db.QueryRow("SELECT * FROM users WHERE username = $1 OR email = $2", body.Credential, body.Credential)
+
+	if err := row.Scan(&user.Id, &user.Email, &user.Name); err == sql.ErrNoRows {
+		w.Write([]byte("User not found!"))
+		return
+	} else if err != nil {
+		fmt.Println(err)
+		errors.InternalServerError(w, r)
+		return
+	}
+
+	jsonRes, err := json.Marshal(user)
+	if err != nil {
+		errors.InternalServerError(w, r)
+		return
+	}
+
+	w.Write(jsonRes)
 }
