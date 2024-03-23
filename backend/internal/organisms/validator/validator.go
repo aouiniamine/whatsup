ed7c4c3ece204c90b3ea850333c7ad4b4cb677a3
amineaouini-notbot <aminaouini565@gmail.com>
2024-03-23 17:03:16 +0100

    setup sending email for validation

--- backend/internal/organisms/validator/validator.go
+++ backend/internal/organisms/validator/validator.go
@@ -4,6 +4,10 @@ import (
 	"crypto/rand"
 	"fmt"
 	"math/big"
+	"net/smtp"
+
+	"github.com/aouiniamine/whatsup/backend/internal/organisms/db"
+	"github.com/aouiniamine/whatsup/backend/internal/organisms/structs"
 )
 
 func randRangeCrypto() (int64, error) {
@@ -19,12 +23,59 @@ func randRangeCrypto() (int64, error) {
 	return n.Int64() + min, nil
 }
 
-func ValidateEmail(email string) error {
-	randomInt, err := randRangeCrypto()
+func ValidateEmail(user structs.User) error {
+	generatedInt, err := randRangeCrypto()
 	if err != nil {
 		return err
 	}
+	fmt.Println("validation int:", generatedInt)
+	var validator int
 
-	fmt.Println(randomInt)
+	db := db.DBConnection
+
+	if err := db.QueryRow(
+		"INSERT INTO connection_req (validator, user_id, req_time) "+
+			"VALUES ($1, $2, CURRENT_TIMESTAMP) RETURNING validator",
+
+		generatedInt, user.Id).Scan(&validator); err != nil {
+		fmt.Println("database error on connection request creation",
+			err.Error())
+
+		return err
+	}
+
+	if err := sendValidationEmail(user.Email, validator); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func sendValidationEmail(receiver string, validationCode int) error {
+
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+	content := fmt.Sprintf("Your validation code is: %d", validationCode)
+
+	sender := fmt.Sprintf("From: <%s>\r\n", From)
+	to := fmt.Sprintf("To: <%s>\r\n", receiver)
+	subject := "Subject: " + content + "\r\n"
+	body := content + "\r\nBye\r\n"
+
+	msg := sender + to + subject + "\r\n" + body
+	fmt.Println(msg)
+	messageFormat := []byte(msg)
+
+	// Authentication.
+	auth := smtp.PlainAuth("", From, Password, smtpHost)
+
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, From, []string{receiver}, messageFormat)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println("Email Sent Successfully!")
 	return nil
 }
